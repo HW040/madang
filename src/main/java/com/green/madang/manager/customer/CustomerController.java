707635583cef5d657dd9e0f4a8d0cfc28ec46a2b
RYU <fbgusdnr9@naver.com>
2024-11-18 17:12:15 +0900

    ffff

--- src/main/java/com/green/madang/manager/customer/CustomerController.java
+++ src/main/java/com/green/madang/manager/customer/CustomerController.java
@@ -1,15 +1,16 @@
 package com.green.madang.manager.customer;
 
 import com.green.madang.common.model.MyResponse;
-import com.green.madang.manager.customer.model.CustomerPostReq;
+import com.green.madang.manager.customer.model.*;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,4 +26,46 @@ public class CustomerController {
         int result = service.postCustomer(p);
         return new MyResponse<>("고객이 등록되었습니다.", result);
     }
+
+    @GetMapping
+    public MyResponse<List<CustomerGetRes>> getCustomerList(@ParameterObject CustomerGetReq p) {
+        List<CustomerGetRes> res = service.getCustomerList(p);
+
+        return new MyResponse<>(p.getPage() + "페이지 데이터", res);
+    }
+
+    //RequestParam을 이용한 GetMapping
+    @GetMapping("/param")
+    @Operation(summary = "고객 리스트2", description = "검색할 내용이 있을 시 검색타입, 검색내용을 모두 보내주어야 한다.")
+    public MyResponse<List<CustomerGetRes>> getCustomerList2( @RequestParam int page
+            , @RequestParam int size
+            , @RequestParam(name="search_type", required = false) String searchType
+            , @RequestParam(name="search_text", required = false) String searchText) {
+        CustomerGetReq p = new CustomerGetReq();
+        p.setPage(page);
+        p.setSize(size);
+        p.setSearchType(searchType);
+        p.setSearchText(searchText);
+        log.info("get-req: {}", p);
+        List<CustomerGetRes> res = service.getCustomerList(p);
+        return new MyResponse<>(p.getPage() + "페이지 데이터", res);
+    }
+
+
+
+    @PutMapping
+    public MyResponse<Integer> updateCustomer(@RequestBody CustomerPutReq p) {
+        int result = service.putCustomer(p);
+        return new MyResponse<>("고객 수정 완료", result);
+    }
+
+    @DeleteMapping
+    public MyResponse<Integer> deleteCustomer(@RequestBody CustomerDelete p) {
+        log.info("request = {}", p.getCustId());
+        return  new MyResponse<>( "고객 삭제", service.deleteCustomer(p));
+    }
+
+
+
+
 }
--- src/main/java/com/green/madang/manager/customer/CustomerService.java
+++ src/main/java/com/green/madang/manager/customer/CustomerService.java
@@ -1,8 +1,10 @@
 package com.green.madang.manager.customer;
 
-import com.green.madang.manager.customer.model.CustomerPostReq;
+import com.green.madang.manager.customer.model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +13,20 @@ public class CustomerService {
 
     public int postCustomer(CustomerPostReq p) {
         return mapper.insCustomer(p);
+    }
+
+    public List<CustomerGetRes> getCustomerList(CustomerGetReq p) {
+
+        p.setStartIdx((p.getPage() -1) * p.getSize());
+        //page 로직 처리
+
+        return mapper.selCustomerList(p);
+    }
+    public int putCustomer(CustomerPutReq p) {
+        return mapper.updCustomer(p);
+    }
+
+    public int deleteCustomer(CustomerDelete p) {
+        return mapper.deleteCustomer(p);
     }
 }
